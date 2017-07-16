f752df17a9454e2e36db67658f00228ea28c0403
droid-dev-7 <jorgedominguezpoblete@gmail.com>
2017-07-16 18:14:21 +0200

    v1

--- app/src/main/java/com/vega/lapitchat/RegisterActivity.java
+++ app/src/main/java/com/vega/lapitchat/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,8 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout mPassword;
     private Button mCreateBtn;
 
+    private Toolbar mToolbar;
+
     //Firebase Auth
     private FirebaseAuth mAuth;
 
@@ -32,6 +35,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Toolbar Set
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Firebase Auth
         mAuth = FirebaseAuth.getInstance();
