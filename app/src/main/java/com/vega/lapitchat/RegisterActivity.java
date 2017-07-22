444790c51ae2e47e0a8bb4a476fdbc237f6c85b8
droid-dev-7 <jorgedominguezpoblete@gmail.com>
2017-07-22 18:13:23 +0200

    v1

--- app/src/main/java/com/vega/lapitchat/RegisterActivity.java
+++ app/src/main/java/com/vega/lapitchat/RegisterActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,6 +30,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolbar;
+
+    private DatabaseReference mDatabase;
 
     //ProgressDialog
 
@@ -65,6 +72,7 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if(!TextUtils.isEmpty(display_name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
 
+
                     mRegProgress.setTitle("Registring User");
                     mRegProgress.setMessage("Please wait while we create your account");
                     mRegProgress.setCanceledOnTouchOutside(false);
@@ -78,7 +86,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String email, String password) {
+    private void register_user(final String display_name, String email, String password) {
 
         mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
@@ -86,12 +94,33 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if(task.isSuccessful()){
 
-                    mRegProgress.dismiss();
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
 
-                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
+                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                    HashMap<String,String> usermap = new HashMap<>();
+                    usermap.put("name",display_name);
+                    usermap.put("status", "Hi there I'm using Lapit Chat App");
+                    usermap.put("image","default");
+                    usermap.put("thumb_image","default");
+
+                    mDatabase.setValue(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+
+                                mRegProgress.dismiss();
+
+                                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+                            }
+
+                        }
+                    });
 
                 }else{
 
